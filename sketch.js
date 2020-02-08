d324e48003633c98bd56d04e4b58885e02d6d924
mecoding4fun <53414760+mecoding4fun@users.noreply.github.com>
2020-02-08 17:32:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function draw() {
   background(0);
 
   if(frameCount % 1 === 0){
-    var raindrop = new Raindrop(random(0, windowWidth), -100, 3, random(20, 50));
+    var raindrop = new Raindrop(random(0, windowWidth), -100, 40, random(20, 50));
     raindrops.push(raindrop);
   }
 
